0aec643a90c539839ec2b9739f3e3d24b76e23ad
Everton miranda de oliveira <evt.miranda@outlook.com>
2017-04-15 14:26:37 -0300

    incluindo lógica de itens adicionais do produto

--- ClassesMarmitex/Produto/DadosAdicionaisProduto.cs
+++ ClassesMarmitex/Produto/DadosAdicionaisProduto.cs
@@ -1,36 +1,62 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ClassesMarmitex
 {
     public class DadosAdicionaisProduto
     {
-        public virtual int Id { get; set; }
-        public virtual int IdProduto { get; set; }
-        public virtual string Nome { get; set; }
-        public virtual decimal? Valor { get; set; }
-        public virtual int OrdemExibicao { get; set; }
-        public virtual bool Ativo { get; set; }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public List<DadosAdicionaisProdutoItem> ItensAdicionais { get; set; }
+        public int QtdMin { get; set; }
+        public int QtdMax { get; set; }
+        public int OrdemExibicao { get; set; }
+        public bool Ativo { get; set; }
     }
 
     [Table("tab_produto_adicional")]
     public class DadosAdicionaisProdutoEntidade
     {
         [Column("id_produto_adicional")]
-        public virtual int Id { get; set; }
-
-        [Column("id_produto")]
-        public virtual int IdProduto { get; set; }
+        public int Id { get; set; }
 
         [Column("nm_adicional")]
-        public virtual string Nome { get; set; }
+        public string Nome { get; set; }
 
-        [Column("vlr_produto_adicional")]
-        public virtual decimal? Valor { get; set; }
+        [Column("nm_descricao")]
+        public string Descricao { get; set; }
+
+        [Column("nr_qtd_min")]
+        public int QtdMin { get; set; }
+
+        [Column("nr_qtd_max")]
+        public int QtdMax { get; set; }
 
         [Column("nr_ordem_exibicao")]
-        public virtual int OrdemExibicao { get; set; }
+        public int OrdemExibicao { get; set; }
 
         [Column("bol_ativo")]
-        public virtual bool Ativo { get; set; }
+        public bool Ativo { get; set; }
     }
+
+    public class DadosAdicionaisProdutoItem
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
+        public bool Ativo { get; set; }
+    }
+
+    [Table("tab_produto_adicional_item")]
+    public class DadosAdicionaisProdutoItemEntidade
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
+        public bool Ativo { get; set; }
+    }
+
 }
