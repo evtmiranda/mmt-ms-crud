4c51c569d01c1d6bd8c1e425916a5b405c230ca3
Everton miranda de oliveira <evt.miranda@outlook.com>
2017-04-27 20:48:48 -0300

    inclusao da classe de criptografia MD5 e as classes de requisicoes e requisicoesrest foram levadas para o projeto classes marmitex

--- ClassesMarmitex/Utils/Criptografia.cs
+++ ClassesMarmitex/Utils/Criptografia.cs
@@ -1,61 +1,26 @@
-﻿using System;
-using System.Text;
-using System.IO;
+﻿using System.Text;
 using System.Security.Cryptography;
 
 namespace ClassesMarmitex
 {
     public static class CriptografiaMD5
     {
-        private static byte[] chave = { };
-        private static byte[] iv = { 12, 34, 56, 78, 90, 102, 114, 126 };
-
-        private static string chaveCriptografia = "MaRmItEx2017SuuuuuUUCESSOWW";
-
-        public static string Criptografar(string valor)
+        public static string GerarHashMd5(string input)
         {
-            byte[] input;
+            MD5 md5Hash = MD5.Create();
+            // Converter a String para array de bytes, que é como a biblioteca trabalha.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
 
-            try
+            // Cria-se um StringBuilder para recompôr a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop para formatar cada byte como uma String em hexadecimal
+            for (int i = 0; i < data.Length; i++)
             {
-                input = Encoding.UTF8.GetBytes(valor);
-                byte[] hashed = MD5.Create().ComputeHash(input);
-                string hash = BitConverter.ToString(hashed).Replace("-", "").ToLower();
-
-                return hash;
+                sBuilder.Append(data[i].ToString("x2"));
             }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
 
-        public static string Descriptografar(string valor)
-        {
-            DESCryptoServiceProvider des;
-            MemoryStream ms;
-            CryptoStream cs; byte[] input;
-
-            try
-            {
-                des = new DESCryptoServiceProvider();
-                ms = new MemoryStream();
-
-                input = new byte[valor.Length];
-                input = Convert.FromBase64String(valor.Replace(" ", "+"));
-
-                chave = Encoding.UTF8.GetBytes(chaveCriptografia.Substring(0, 8));
-
-                cs = new CryptoStream(ms, des.CreateDecryptor(chave, iv), CryptoStreamMode.Write);
-                cs.Write(input, 0, input.Length);
-                cs.FlushFinalBlock();
-
-                return Encoding.UTF8.GetString(ms.ToArray());
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
+            return sBuilder.ToString();
         }
     }
 }
---
+++ ClassesMarmitex/Utils/Requisicoes.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net;
+
+namespace ClassesMarmitex
+{
+    public class Requisicoes
+    {
+        private RequisicoesREST rest;
+
+        //construtor da classe recebe um RequisicoesREST
+        //O Ninject é o responsável por cuidar da criação de todos esses objetos
+        public Requisicoes(RequisicoesREST rest)
+        {
+            this.rest = rest;
+        }
+
+        public List<MenuCardapio> ListarMenuCardapio(int idParceiro)
+        {
+            DadosRequisicaoRest retornoGet = new DadosRequisicaoRest();
+
+            List<MenuCardapio> listaMenuCardapio;
+
+            retornoGet = rest.Get("/menucardapio/listar/" + idParceiro);
+
+            if (retornoGet.HttpStatusCode != HttpStatusCode.OK)
+                return null;
+
+            string json = retornoGet.objeto.ToString();
+
+            listaMenuCardapio = JsonConvert.DeserializeObject<List<MenuCardapio>>(json);
+
+            return listaMenuCardapio;
+        }
+
+        public List<Produto> ListarProdutos()
+        {
+            DadosRequisicaoRest retornoGet = new DadosRequisicaoRest();
+
+            List<Produto> listaProdutos;
+
+            retornoGet = rest.Get("/produto/listar");
+
+            if (retornoGet.HttpStatusCode != HttpStatusCode.OK)
+                return null;
+
+            string json = retornoGet.objeto.ToString();
+
+            listaProdutos = JsonConvert.DeserializeObject<List<Produto>>(json);
+
+            return listaProdutos;
+        }
+
+        public List<FormaDePagamento> ListarFormasPagamento(int idParceiro)
+        {
+            DadosRequisicaoRest retornoGet = new DadosRequisicaoRest();
+
+            List<FormaDePagamento> listaFormaPagamento;
+
+            retornoGet = rest.Get("/formaPagamento/listar/" + idParceiro);
+
+            if (retornoGet.HttpStatusCode != HttpStatusCode.OK)
+                return null;
+
+            string json = retornoGet.objeto.ToString();
+
+            listaFormaPagamento = JsonConvert.DeserializeObject<List<FormaDePagamento>>(json);
+
+            return listaFormaPagamento;
+        }
+    }
+}
