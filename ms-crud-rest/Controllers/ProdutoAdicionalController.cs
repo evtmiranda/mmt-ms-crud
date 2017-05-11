f16477fd18ace1eccbccb3b3a28f80ff53578256
Everton miranda de oliveira <evt.miranda@outlook.com>
2017-05-11 19:22:34 -0300

    configuracao da view index de produto adicional

---
+++ ms-crud-rest/Controllers/ProdutoAdicionalController.cs
@@ -0,0 +1,46 @@
+﻿using ClassesMarmitex;
+using ms_crud_rest.DAO;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ms_crud_rest.Controllers
+{
+    public class ProdutoAdicionalController : ApiController
+    {
+        private ProdutoAdicionalDAO produtoAdicionalDAO;
+        private LogDAO logDAO;
+
+        //construtor do controller, recebe um produtoDAO e um logDAO, que por sua vez recebe uma ISession.
+        //O Ninject é o responsável por cuidar da criação de todos esses objetos
+        public ProdutoAdicionalController(ProdutoAdicionalDAO produtoAdicionalDAO, LogDAO logDAO)
+        {
+            this.produtoAdicionalDAO = produtoAdicionalDAO;
+            this.logDAO = logDAO;
+        }
+
+        /// <summary>
+        /// Retorna todos os produtos existentes dentro do cardápio enviado como parâmetro
+        /// </summary>
+        /// <param name="idMenuCardapio">Id do cardápio ao qual os produtos pertencem</param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/ProdutoAdicional/Listar/{idLoja}")]
+        public HttpResponseMessage ListarProdutos(int idLoja)
+        {
+            try
+            {
+                IList<DadosProdutoAdicional> produtosAdicionais = produtoAdicionalDAO.Listar(idLoja);
+                return Request.CreateResponse(HttpStatusCode.OK, produtosAdicionais);
+            }
+            catch (Exception)
+            {
+                string mensagem = string.Format("ocorreu um problema ao buscar os produtos adicionais. por favor, tente atualizar a página ou acessar dentro de alguns minutos...");
+                HttpError error = new HttpError(mensagem);
+                return Request.CreateResponse(HttpStatusCode.NotFound, error);
+            }
+        }
+    }
+}
---
+++ ms-crud-rest/DAO/ProdutoAdicionalDAO.cs
@@ -0,0 +1,105 @@
+﻿using ClassesMarmitex;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ms_crud_rest.DAO
+{
+    public class ProdutoAdicionalDAO : GenericDAO<DadosProdutoAdicional>
+    {
+        public ProdutoAdicionalDAO(SqlServer sqlConn, LogDAO logDAO) : base(sqlConn, logDAO) { }
+
+        public override List<DadosProdutoAdicional> Listar(int idLoja)
+        {
+            List<DadosProdutoAdicionalEntidade> listaProdutoAdicionalEntidade = new List<DadosProdutoAdicionalEntidade>();
+            List<DadosProdutoAdicional> listaProdutoAdicional = new List<DadosProdutoAdicional>();
+
+            List<DadosProdutoAdicionalItemEntidade> listaProdutoAdicionalItemEntidade = new List<DadosProdutoAdicionalItemEntidade>();
+            List<DadosProdutoAdicionalItem> listaProdutoAdicionalItem = new List<DadosProdutoAdicionalItem>();
+
+            try
+            {
+                sqlConn.StartConnection();
+
+
+                #region Produtos Adicionais
+                //busca os produtos
+                sqlConn.Command.CommandType = System.Data.CommandType.Text;
+                sqlConn.Command.CommandText = string.Format(@"SELECT
+	                                                            id_produto_adicional,
+	                                                            nm_adicional,
+	                                                            nm_descricao,
+	                                                            bol_ativo
+                                                            FROM tab_produto_adicional
+                                                            WHERE id_loja = @id_loja;");
+
+                sqlConn.Command.Parameters.Clear();
+                sqlConn.Command.Parameters.AddWithValue("@id_loja", idLoja);
+                sqlConn.Reader = sqlConn.Command.ExecuteReader();
+
+                //transforma a entidade em objeto
+                listaProdutoAdicionalEntidade = new ModuloClasse().PreencheClassePorDataReader<DadosProdutoAdicionalEntidade>(sqlConn.Reader);
+                foreach (var produtoAdicionalEntidade in listaProdutoAdicionalEntidade)
+                {
+                    listaProdutoAdicional.Add(produtoAdicionalEntidade.ToProdutoAdicional());
+                }
+
+                //limpa os dados da execução anterior
+                sqlConn.Command.CommandText = "";
+                if (sqlConn.Reader != null)
+                    sqlConn.Reader.Close();
+
+                #endregion
+
+                #region Itens Adicionais Produtos
+                //lista quais itens os produtos adicionais possui
+                sqlConn.Command.CommandText = @"SELECT
+	                                                id_produto_adicional_item,
+	                                                id_produto_adicional,
+	                                                nm_adicional_item,
+	                                                nm_descricao_item,
+	                                                vlr_adicional_item,
+	                                                bol_ativo
+                                                FROM tab_produto_adicional_item;";
+
+                sqlConn.Reader = sqlConn.Command.ExecuteReader();
+
+                //transforma a entidade em objeto
+                listaProdutoAdicionalItemEntidade = new ModuloClasse().PreencheClassePorDataReader<DadosProdutoAdicionalItemEntidade>(sqlConn.Reader);
+
+                foreach (var itemAdicional in listaProdutoAdicionalItemEntidade)
+                {
+                    listaProdutoAdicionalItem.Add(itemAdicional.ToProdutoAdicionalItem());
+                }
+
+                //limpa os dados da execução anterior
+                sqlConn.Command.CommandText = "";
+                if (sqlConn.Reader != null)
+                    sqlConn.Reader.Close();
+
+                #endregion
+
+                //Adiciona os itens adicionais aos produtos adicionais
+                for (int i = 0; i < listaProdutoAdicional.Count; i++)
+                {
+                    listaProdutoAdicional[i].ItensAdicionais =
+                        listaProdutoAdicionalItem.Where(p => p.IdProdutoAdicional == listaProdutoAdicional[i].Id).ToList();
+                }
+
+                return listaProdutoAdicional;
+            }
+            catch (Exception ex)
+            {
+                logDAO.Adicionar(new Log { Mensagem = "erro ao buscar os produtos adicionais", Descricao = ex.Message, StackTrace = ex.StackTrace == null ? "" : ex.StackTrace });
+
+                throw ex;
+            }
+            finally
+            {
+                sqlConn.Command.Parameters.Clear();
+                sqlConn.CloseConnection();
+            }
+        }
+
+    }
+}
