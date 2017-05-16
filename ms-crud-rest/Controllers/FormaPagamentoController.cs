a4a2ee5110c8eced298401c65aed77db17b33c02
Everton miranda de oliveira <evt.miranda@outlook.com>
2017-05-16 09:03:42 -0300

    configuracao das formas de pagamento

--- ms-crud-rest/Controllers/FormaPagamentoController.cs
+++ ms-crud-rest/Controllers/FormaPagamentoController.cs
@@ -21,11 +21,58 @@ namespace ms_crud_rest.Controllers
             this.logDAO = logDAO;
         }
 
-        /// <summary>
-        /// Retorna uma lista de forma de pagamento para a loja enviada como parâmetro
-        /// </summary>
-        /// <param name="idLoja">Id da loja para a qual se quer as formas de pagamento</param>
-        /// <returns></returns>
+        [HttpPost]
+        [Route("api/FormaPagamento/Adicionar")]
+        public HttpResponseMessage Adicionar([FromBody] FormaDePagamento formaPagamento)
+        {
+            try
+            {
+                formaPagamentoDAO.Adicionar(formaPagamento);
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created);
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                string mensagem = string.Format("nao foi possivel cadastrar a forma de pagamento. erro: {0}", ex);
+                HttpError error = new HttpError(mensagem);
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, error);
+            }
+        }
+
+        [HttpGet]
+        [Route("api/FormaPagamento/{id}")]
+        public HttpResponseMessage Buscar(int id)
+        {
+            try
+            {
+                FormaDePagamento pagamento = new FormaDePagamento();
+
+                pagamento = formaPagamentoDAO.BuscarPorId(id);
+
+                if (pagamento == null)
+                    throw new KeyNotFoundException();
+
+                return Request.CreateResponse(HttpStatusCode.OK, pagamento);
+            }
+            catch (KeyNotFoundException)
+            {
+                return Request.CreateResponse(HttpStatusCode.NoContent);
+            }
+            catch (ParceiroNaoEncontradoException)
+            {
+                return Request.CreateResponse(HttpStatusCode.NoContent);
+            }
+            catch (Exception)
+            {
+                string mensagem = "Não foi possível consultar a forma de pagamento. Por favor, tente novamente ou entre em contato com o administrador do sistema";
+                HttpError error = new HttpError(mensagem);
+
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, error);
+            }
+        }
+
         [HttpGet]
         [Route("api/FormaPagamento/Listar/{idLoja}")]
         public HttpResponseMessage ListarFormasPagamento(int idLoja)
@@ -49,9 +96,45 @@ namespace ms_crud_rest.Controllers
             }
         }
 
-        //public FormaDePagamento BuscarFormaPagamento(string nomeFormaPagamento, int idParceiro)
-        //{
-        //    return formaPagamentoDAO.BuscarPorNome(nomeFormaPagamento, idParceiro);
-        //}
+
+        [HttpPost]
+        [Route("api/FormaPagamento/Atualizar")]
+        public HttpResponseMessage Atualizar([FromBody] FormaDePagamento formaPagamento)
+        {
+            try
+            {
+                formaPagamentoDAO.Atualizar(formaPagamento);
+
+                return Request.CreateResponse(HttpStatusCode.OK, formaPagamento);
+            }
+            catch (Exception)
+            {
+                string mensagem = "Não foi possível atualizar a forma de pagamento. Por favor, tente novamente ou entre em contato com o administrador do sistema";
+                HttpError error = new HttpError(mensagem);
+
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, error);
+            }
+        }
+
+
+        [HttpPost]
+        [Route("api/FormaPagamento/Excluir")]
+        public HttpResponseMessage Excluir([FromBody] FormaDePagamento formaPagamento)
+        {
+            try
+            {
+                formaPagamentoDAO.Excluir(formaPagamento);
+
+                return Request.CreateResponse(HttpStatusCode.OK, formaPagamento);
+            }
+            catch (Exception)
+            {
+                string mensagem = "Não foi possível excluir a forma de pagamento. Por favor, tente novamente ou entre em contato com o administrador do sistema";
+                HttpError error = new HttpError(mensagem);
+
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, error);
+            }
+        }
+
     }
 }
--- ms-crud-rest/DAO/FormaPagamentoDAO.cs
+++ ms-crud-rest/DAO/FormaPagamentoDAO.cs
@@ -9,6 +9,57 @@ namespace ms_crud_rest.DAO
     {
         public FormaPagamentoDAO(SqlServer sqlConn, LogDAO logDAO) : base(sqlConn, logDAO) { }
 
+        /// <summary>
+        /// Faz a busca de uma forma de pagamento
+        /// </summary>
+        /// <param name="idParceiro">id da forma de pagamento</param>
+        /// <returns></returns>
+        public override FormaDePagamento BuscarPorId(int id)
+        {
+            FormaDePagamento pagamento;
+            List<FormaDePagamentoEntidade> listaPagamentoEntidade;
+
+            try
+            {
+                sqlConn.StartConnection();
+
+                sqlConn.Command.CommandType = System.Data.CommandType.Text;
+                sqlConn.Command.CommandText = string.Format(@"SELECT
+	                                                            id_forma_pagamento,
+	                                                            id_loja,
+	                                                            nm_forma_pagamento,
+	                                                            bol_ativo
+                                                            FROM tab_forma_pagamento
+                                                            WHERE id_forma_pagamento = @id_forma_pagamento");
+
+                sqlConn.Command.Parameters.AddWithValue("@id_forma_pagamento", id);
+
+                sqlConn.Reader = sqlConn.Command.ExecuteReader();
+
+                listaPagamentoEntidade = new ModuloClasse().PreencheClassePorDataReader<FormaDePagamentoEntidade>(sqlConn.Reader);
+
+                if (listaPagamentoEntidade.Count == 0)
+                    throw new KeyNotFoundException();
+
+                pagamento = listaPagamentoEntidade[0].ToFormaPagamento();
+
+                //fecha o reader
+                sqlConn.Reader.Close();
+
+                return pagamento;
+            }
+            catch (Exception ex)
+            {
+                logDAO.Adicionar(new Log { Mensagem = "Erro ao buscar a forma de pagamento", Descricao = ex.Message, StackTrace = ex.StackTrace == null ? "" : ex.StackTrace });
+                throw ex;
+            }
+            finally
+            {
+                sqlConn.CloseConnection();
+                sqlConn.Reader.Close();
+            }
+        }
+
         public override List<FormaDePagamento> Listar(int idLoja)
         {
             List<FormaDePagamentoEntidade> listaPagamentoEntidade = new List<FormaDePagamentoEntidade>();
@@ -25,9 +76,7 @@ namespace ms_crud_rest.DAO
 	                                                            nm_forma_pagamento,
 	                                                            bol_ativo
                                                             FROM tab_forma_pagamento
-                                                            WHERE id_loja = @id_loja
-                                                            AND bol_ativo = 1;");
-
+                                                            WHERE id_loja = @id_loja");
 
 
                 sqlConn.Command.Parameters.AddWithValue("@id_loja", idLoja);
@@ -119,6 +168,109 @@ namespace ms_crud_rest.DAO
                 sqlConn.CloseConnection();
                 sqlConn.Reader.Close();
                 sqlConn.Command.Parameters.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Faz o cadastro de uma forma de pagamento
+        /// </summary>
+        /// <param name="formaPagamento">dados</param>
+        /// <returns></returns>
+        public override void Adicionar(FormaDePagamento formaPagamento)
+        {
+            try
+            {
+                sqlConn.StartConnection();
+                sqlConn.Command.CommandType = System.Data.CommandType.Text;
+
+                sqlConn.Command.CommandText = string.Format(@"INSERT INTO tab_forma_pagamento(id_loja, nm_forma_pagamento, bol_ativo)
+                                                                VALUES(@id_loja, @nm_forma_pagamento, @bol_ativo);");
+
+                sqlConn.Command.Parameters.AddWithValue("@id_loja", formaPagamento.IdLoja);
+                sqlConn.Command.Parameters.AddWithValue("@nm_forma_pagamento", formaPagamento.Nome);
+                sqlConn.Command.Parameters.AddWithValue("@bol_ativo", formaPagamento.Ativo);
+
+                sqlConn.Command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                logDAO.Adicionar(new Log { Mensagem = "Erro ao cadastrar a forma de pagamento", Descricao = ex.Message, StackTrace = ex.StackTrace == null ? "" : ex.StackTrace });
+                throw ex;
+            }
+            finally
+            {
+                sqlConn.CloseConnection();
+            }
+        }
+
+        /// <summary>
+        /// Atualiza os dados de uma forma de pagametno
+        /// </summary>
+        /// <param name="formaPagamento">formaPagamento que será atualizada</param>
+        /// <returns></returns>
+        public override void Atualizar(FormaDePagamento formaPagamento)
+        {
+            try
+            {
+                sqlConn.StartConnection();
+                sqlConn.Command.CommandType = System.Data.CommandType.Text;
+
+                sqlConn.Command.Parameters.AddWithValue("@nm_forma_pagamento", formaPagamento.Nome);
+                sqlConn.Command.Parameters.AddWithValue("@bol_ativo", formaPagamento.Ativo);
+                sqlConn.Command.Parameters.AddWithValue("@id_forma_pagamento", formaPagamento.Id);
+
+                sqlConn.Command.CommandText = string.Format(@"UPDATE tab_forma_pagamento
+	                                                            SET nm_forma_pagamento = @nm_forma_pagamento,
+		                                                            bol_ativo = @bol_ativo
+                                                            WHERE id_forma_pagamento = @id_forma_pagamento;");
+
+                sqlConn.Command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                logDAO.Adicionar(new Log { Mensagem = "Erro ao atualizar a forma de pagamento", Descricao = ex.Message, StackTrace = ex.StackTrace == null ? "" : ex.StackTrace });
+
+                throw ex;
+            }
+            finally
+            {
+                sqlConn.CloseConnection();
+            }
+        }
+
+        /// <summary>
+        /// Seta uma forma de pagamento como inativo
+        /// </summary>
+        /// <param name="formaPagamento">formaPagamento que será inativada</param>
+        /// <returns></returns>
+        public override void Excluir(FormaDePagamento formaPagamento)
+        {
+            try
+            {
+                sqlConn.StartConnection();
+
+                sqlConn.Command.CommandType = System.Data.CommandType.Text;
+
+                sqlConn.Command.Parameters.AddWithValue("@bol_ativo", formaPagamento.Ativo);
+                sqlConn.Command.Parameters.AddWithValue("@id_forma_pagamento", formaPagamento.Id);
+
+                sqlConn.Command.CommandText = string.Format(@"UPDATE tab_forma_pagamento
+	                                                            SET bol_ativo = @bol_ativo
+                                                            WHERE id_forma_pagamento = @id_forma_pagamento;");
+
+                sqlConn.Command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                logDAO.Adicionar(new Log { Mensagem = "Erro ao atualizar a forma de pagamento", Descricao = ex.Message, StackTrace = ex.StackTrace == null ? "" : ex.StackTrace });
+
+                throw ex;
+            }
+            finally
+            {
+                sqlConn.CloseConnection();
             }
         }
     }
