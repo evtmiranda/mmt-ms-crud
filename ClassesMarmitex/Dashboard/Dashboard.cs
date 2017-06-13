5aa0418a4dc70bc4b02a41a6fc3fa83eb3d654b7
Everton miranda de oliveira <evt.miranda@outlook.com>
2017-06-13 20:44:05 -0300

    inclusao do nome das colunas na classe dashboard

--- ClassesMarmitex/Dashboard/Dashboard.cs
+++ ClassesMarmitex/Dashboard/Dashboard.cs
@@ -1,12 +1,25 @@
-﻿namespace ClassesMarmitex
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClassesMarmitex
 {
     public class Dashboard
     {
+        [Column("Pedidos")]
         public int Pedidos { get; set; }
-        public int Vendas { get; set; }
+
+        [Column("Vendas")]
+        public decimal Vendas { get; set; }
+
+        [Column("Visitas")]
         public int Visitas { get; set; }
+
+        [Column("Parceiros")]
         public int Parceiros { get; set; }
+
+        [Column("PedidosAtrasados")]
         public int PedidosAtrasados { get; set; }
+
+        [Column("PedidosEntregues")]
         public int PedidosEntregues { get; set; }
     }
 }
